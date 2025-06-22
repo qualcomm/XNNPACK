9ee8f5c0c701ec550680660f43dded802f369b2e
Vaisakh K V <quic_vaiskv@quicinc.com>
2025-06-22 14:02:20 +0530

    Updated kleidiai commit and patch

--- cmake/DownloadKleidiAI.cmake
+++ cmake/DownloadKleidiAI.cmake
@@ -18,12 +18,14 @@ ENDIF()
 # LINT.IfChange
 INCLUDE(ExternalProject)
 ExternalProject_Add(kleidiai
-  URL https://gitlab.arm.com/kleidi/kleidiai/-/archive/847ebd19d0192528659b0a0fa2c6057eed674c6a/kleidiai-847ebd19d0192528659b0a0fa2c6057eed674c6a.zip
-  URL_HASH SHA256=f3ea4fce53f3b31076958dbff229f0048dae15bf454929673c78292a56279d52
+        #URL https://gitlab.arm.com/kleidi/kleidiai/-/archive/847ebd19d0192528659b0a0fa2c6057eed674c6a/kleidiai-847ebd19d0192528659b0a0fa2c6057eed674c6a.zip
+  URL https://gitlab.arm.com/kleidi/kleidiai/-/archive/73d1e85dba909be752031407cc97ca0b3a614343/kleidiai-73d1e85dba909be752031407cc97ca0b3a614343.zip
+  #  URL_HASH SHA256=ac8c946803bed3fa2e53bc88284f9d07787b9aa4
   SOURCE_DIR "${CMAKE_BINARY_DIR}/kleidiai-source"
   BINARY_DIR "${CMAKE_BINARY_DIR}/kleidiai"
   CONFIGURE_COMMAND ""
-  PATCH_COMMAND ""
+  PATCH_COMMAND ${CMAKE_COMMAND} -E echo "Applying patch..." && 
+  ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> patch -p1 < /local/mnt/workspace/vaiskv/sme/onnx/integration/public/onnxruntime/cmake/patches/xnnpack/kleidiai-sme1.patch
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
   TEST_COMMAND ""
